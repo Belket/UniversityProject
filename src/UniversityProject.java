8b2ee4f23cae37c9a705546899baabb5f5056139
Gleb <glebchek@rambler.ru>
2020-03-14 11:10:25 +0300

    new project update

--- src/UniversityProject.java
+++ src/UniversityProject.java
@@ -1,7 +1,29 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Array;
+import java.util.ArrayList;
 
 public class UniversityProject {
     public static void main(String[] args) {
 
+        ArrayList<String> warriors = new ArrayList<>();
+        try(BufferedReader br = new BufferedReader(new FileReader("init.txt")))
+        {
+            //чтение построчно
+            String read_string;
+            while((read_string=br.readLine()) != null){
+                warriors.add(read_string);
+            }
+        }
+        catch(IOException ex){
+
+            System.out.println(ex.getMessage());
+        }
+
+        for(int i=0; i < warriors.size(); i += 6){
+            
+        }
         Warrior warrior = new Warrior(1000, 100, 85, 185, 12, 1.2);
         System.out.println(warrior.physical_attack());
     }
